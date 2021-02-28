de8505ce3cd2477ab0042634bc400af2a038f17c
Rilton SM <rilton_moreira@hotmail.com>
2021-02-28 16:44:08 -0300

    feat: aplicação 1.0

---
+++ src/components/Profile.tsx
@@ -0,0 +1,22 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import styles from '../styles/components/Profile.module.css';
+
+function Profile () {
+    const { level } = useContext(ChallengesContext);
+    return(
+        <div className={styles.profileContainer}>
+            <img src="https://github.com/RiltonSM.png" alt="Rilton SM"/>
+            <div>
+                <strong>Rilton SM</strong>
+                <p>
+                    <img src="icons/level.svg" alt="level"/>
+                    Level {level}
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
+
